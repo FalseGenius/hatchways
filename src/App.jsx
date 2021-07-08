a57b111adf53f6b31f6057e5e874bcc66b7ee7f1
FalseGenius <billionairejackal@gmail.com>
2021-07-08 19:09:35 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,131 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+
+const axios = require('axios');
+
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  company: string;
+  skill: string;
+  average: number;
+}
+
+interface UserPicture {
+  pic: string;
+}
+
+interface UserInformation {
+  name: User;
+  pic: UserPicture;
+}
+
+const fetchRandomData = () => {
+  return axios.get('https://api.hatchways.io/assessment/students').then(({data}) => {
+    return (data);
+  })
+}
+
+const getUserName = (userInfos: UserInformation) => {
+  return `${userInfos.firstName} ${userInfos.lastName} `;
+}
+
+const otherDetails = (userInfos) => {
+  return <div>
+            <p>Email: {userInfos.email}</p>
+            <p>Company: {userInfos.company}</p>
+            <p>Skill: {userInfos.skill}</p>
+
+          </div>
+}
+
+const average = (arr) => {
+  let avg = 0;
+  arr.map((item, idx) => {
+    avg += parseInt(item);
+  });
+
+  return `Average: ${avg / arr.length}%`;
+}
+
+const showGrades = (grades) => {
+  grades.map((item, idx) => (
+    console.log(item)
+  ));
+};
+
+function App() {
+
+  const [fetchUserData, setfetchUserData] = useState('');
+  const [userInfo, setUserInfo] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    fetchRandomData().then(randomData => {
+      setfetchUserData(JSON.stringify(randomData) || 'error');
+      setUserInfo(randomData.students);
+      setSearchResult(randomData.students);
+    })
+  }, [])
+
+
+  const handleChange = (event) => {
+    let value = event.target.value;
+    setSearch(value);
+
+    if (value !== '') {
+      const newList = userInfo.filter((item) => {
+        return Object.values(item.firstName).concat(' ').concat(item.lastName).join('').toLowerCase().includes(value.toLowerCase());
+      })
+      setSearchResult(newList);
+    } else {
+      setSearchResult(userInfo);
+    }
+  };
+
+  return (
+    <>
+
+      <h1 style={{textAlign: "center"}}>Hello World</h1>
+      <pre>{fetchUserData}</pre>
+      <div>
+          <div className='search'>
+
+            <div className='ui icon input'>
+              <input onChange={handleChange} type="text" placeholder="Search by name" ></input>
+            </div>
+
+          </div>
+          <div>
+            {
+              searchResult.map((item: User, idx) => (
+                <div style={{marginBottom: '70px'}} key={idx}>
+
+                  <div style={{float: 'left', paddingTop: '50px'}}>
+                    <img style={{margin: '20px', height: '160px', weight: '200px', border: '1px solid black', borderRadius: "50%", color: 'black'}} src={item.pic} />
+                  </div>
+                  <button  style={{width: '30px', height: '30px' ,float: 'right', marginTop: '30px', border: 'none', backgroundColor: 'gray'}}>+</button>
+                  <div style={{display: 'inline-block'}}>
+
+                    <p style={{fontWeight: '500', fontSize: '30px'}}>{getUserName(item)}</p>
+                    <p>{otherDetails(item)}</p>
+                    <p>{average(item.grades)}</p>
+                    <ul style={{listStyleType: 'none', padding: '0', margin: '0'}}>
+                      {item.grades.map((item, idx) => (
+                        <li>{`Test ${idx+1}: ${parseInt(item)}%`}</li>
+                    ))}</ul>
+                  </div>
+                  <hr style={{color: 'gray', marginTop: '23px'}} />
+                </div>
+
+                ))
+            }
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
